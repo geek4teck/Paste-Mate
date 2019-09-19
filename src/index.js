565e23bda3dd06def8cac591f5823a99871fbb2c
Fareed <fareed@clouwood.com>
2019-09-19 10:45:01 +0530

    worked on css

--- src/index.js
+++ src/index.js
@@ -4,23 +4,33 @@ import React from 'react';
 const heading = {
     textAlign: 'center',
     marginTop: '50px',
-    fontFamily: 'sans-serif',
+    fontFamily: 'Questrial',
     width: '100%',
-    fontSize: '40px'
+    fontSize: '40px',
+    color: '#fff',
+    textShadow: '2px 1px 0 #2e2e2e'
+    
 };
 const subHeading = {
     textAlign: 'center',
+    margin: '0 auto',
     marginTop: '50px',
-    fontFamily: 'monospace',
-    width: '100%'
+    fontFamily: 'Questrial',
+    width: '36%',
+    color: '#fff',
+    borderBottom: '1px solid #fff',
+    paddingBottom: '5px',
+    letterSpacing: '0.5px'
 };
+
 const textBox = {
     width: '80%',
     height: '200px',
     marginTop: '50px',
-    fontFamily: 'serif',
+    fontFamily: 'Questrial',
     fontSize: '17px',
-    resize: 'none'
+    resize: 'none',
+    borderRadius:'10px'
 };
 const textareaContainer = {
     textAlign: 'center'
@@ -32,8 +42,17 @@ const buttonContainer = {
     marginTop: '30px'
 }
 const saveChangesBtn = {
-    height: '38px'
+      borderRadius: '25px',
+    backgroundColor: 'transparent',
+    border: '3px solid #fff',
+    padding: '10px 30px',
+    color:'#fff',
+    fontFamily: 'Questrial'
 }
+const bod = {
+    backgroundColor: 'red'
+}
+
 
 function Main() {
     return <div>
